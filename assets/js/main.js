13b1c92104fe3cf559861c75775886b5f71be770
Aizhee <108914551+Aizhee@users.noreply.github.com>
2024-04-20 15:54:57 +0800

    Added collapsible animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -75,30 +75,31 @@ document.querySelectorAll('.nav-btn').forEach(function(btn) {
 });
 
 var coll = document.getElementsByClassName("collapsible");
-var i;
 
-for (i = 0; i < coll.length; i++) {
+for (var i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-      this.classList.remove("active");
-      this.querySelector('i').classList.remove("fa-chevron-up");
-        this.querySelector('i').classList.add("fa-chevron-down");
+    var icon = this.querySelector('i');
+
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+      icon.classList.remove("fa-chevron-up");
+      icon.classList.add("fa-chevron-down");
     } else {
       for (var j = 0; j < coll.length; j++) {
         coll[j].classList.remove("active");
-        coll[j].nextElementSibling.style.display = "none";
+        coll[j].nextElementSibling.style.maxHeight = null;
         coll[j].querySelector('i').classList.remove("fa-chevron-up");
-          coll[j].querySelector('i').classList.add("fa-chevron-down");
+        coll[j].querySelector('i').classList.add("fa-chevron-down");
       }
-      content.style.display = "block";
+      content.style.maxHeight = content.scrollHeight + "px";
       this.classList.add("active");
-      this.querySelector('i').classList.remove("fa-chevron-down");
-        this.querySelector('i').classList.add("fa-chevron-up");
+      icon.classList.remove("fa-chevron-down");
+      icon.classList.add("fa-chevron-up");
     }
   });
 }
+
 
 document.addEventListener('DOMContentLoaded', function() {
     fetch('https://api.github.com/repos/Aizhee/online-tech/commits')
